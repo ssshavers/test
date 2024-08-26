46efb33124103f02170201e3e8d0c2ffa482d2f7
Sydney Shavers <ssshavers@valdosta.edu>
2024-08-26 13:59:44 -0400

    Updated

--- ver2/ver2/Account.java
+++ ver2/ver2/Account.java
@@ -7,10 +7,7 @@ public class Account {
 	private double balance;
 	private String name;
 	
-	// Constructor
-public Account(double balance) {
-	this("Unknown", balance);
-}
+	
 public Account(String name, double balance) {
 	this.name = name;
 	this.balance = balance;
